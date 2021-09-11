4e7c7b59847783f532b613b445ac5ab2d1104904
M4TH3US17 <matheusdalvino50@gmail.com>
2021-09-11 15:45:55 -0400

    Project created

---
+++ src/test/java/com/matheus/client/RoomReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheus.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
