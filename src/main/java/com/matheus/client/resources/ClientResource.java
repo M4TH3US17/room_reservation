2832801ed45427e51af596df7d1f53c3416dd9d6
M4TH3US17 <matheusdalvino50@gmail.com>
2021-10-04 15:53:04 -0400

    client delete

--- src/main/java/com/matheus/client/resources/ClientResource.java
+++ src/main/java/com/matheus/client/resources/ClientResource.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,7 +30,6 @@ public class ClientResource {
 		return ResponseEntity.ok().body(list);
 	}
 
-
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Client> findById(@PathVariable Long id){
 		Client obj = service.findById(id);
@@ -42,5 +42,11 @@ public class ClientResource {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
 				.buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).body(obj);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }	
