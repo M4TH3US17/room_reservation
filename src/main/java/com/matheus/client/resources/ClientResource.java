cb111d05a7cdd0ff0f8cb1cc75f2673f52b294b4
M4TH3US17 <matheusdalvino50@gmail.com>
2021-10-05 00:36:52 -0400

    client update

--- src/main/java/com/matheus/client/resources/ClientResource.java
+++ src/main/java/com/matheus/client/resources/ClientResource.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -49,4 +50,11 @@ public class ClientResource {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Client> update(@PathVariable Long id, @RequestBody Client obj){
+		obj = service.update(id, obj);
+		return ResponseEntity.ok().body(obj);
+	}
+	
 }	
--- src/main/java/com/matheus/client/services/ClientService.java
+++ src/main/java/com/matheus/client/services/ClientService.java
@@ -31,4 +31,16 @@ public class ClientService {
 	public void delete(Long id) {
 		repository.deleteById(id);
 	}
+	
+	public Client update(Long id, Client obj) {
+		Client entity = repository.getById(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void updateData(Client entity, Client obj) {
+		entity.setName(obj.getName());
+		entity.setContact(obj.getContact());	
+		}
+		
 }
